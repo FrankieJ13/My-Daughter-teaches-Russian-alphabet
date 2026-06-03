cf20fa67b4f8b068a53e69931ec944a1c3d9afc2
FrankieJ13 <frankie13.jung@gmail.com>
2026-06-03 15:43:36 +0500

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/My-Daughter-teaches-Russian-alphabet/sw.js', { scope: '/My-Daughter-teaches-Russian-alphabet/' })})}
